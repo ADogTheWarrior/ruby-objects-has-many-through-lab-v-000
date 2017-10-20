be2dcf3e65a8a3306b813a78806c916a94fbfb43
Adam Fudge <coolfudge@gmail.com>
2017-10-20 21:38:55 +0000

    automatically backed up by learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,11 +1,11 @@
 # class Genre
 #   attr_accessor :name, :songs
-# 
+#
 #   def initialize(name)
 #     @name = name
 #     @songs = []
 #   end
-# 
+#
 #   def artists
 #     artists = []
 #     @songs.each do |song|
@@ -13,31 +13,31 @@
 #     end
 #     artists.uniq
 #   end
-#   
+#
 #   def add_song(song)
 #     add song to genre
 #   end
-#   
+#
 #   def songs
 #     @songs
 #   end
 # end
 class Genre
   attr_accessor :name
- 
+
   def initialize(name)
     @name = name
     @songs = []
   end
- 
+
   def songs
     @songs
   end
- 
+
   def add_song(song)
     @songs << song
   end
- 
+
   def artists
     @songs.collect do |song|
       song.artist
