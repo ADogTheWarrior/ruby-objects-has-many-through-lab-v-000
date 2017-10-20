92355d508d1a6793b8e8886b513618be59e4a09a
Adam Fudge <coolfudge@gmail.com>
2017-10-20 21:50:00 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,12 +1,14 @@
 class Patient
   attr_accessor :name
   @appointments = []
-
+  
   def initialize(name)
     @name = name
   end
 
   def add_appointment(appointment)
+    @appointments << appointment
+    appointment.patient = self
   end
 
   def doctors
